45540903c9347c6d02ee5bf83c5d838079cd1dc0
Adam_Berger <akberger@brandeis.edu>
2016-01-28 14:28:44 -0500

    Added some features

--- extract_features.py
+++ extract_features.py
@@ -1,5 +1,6 @@
 import sys
 from collections import defaultdict
+from nltk.corpus import names, gazetteers
 
 """ Read in twitter data and extract features for each word. 
 Data files have one word per line.
@@ -21,6 +22,16 @@ class FeatureExtractor(object):
     def __init__(self, corpus):
         self.corpus = corpus # path to corpus
         self.worddict = defaultdict(int)
+
+        self.names = set([name for name in names.words('male.txt')] +
+                         [name for name in names.words('female.txt')])
+
+        self.places = set([ca_prov for ca_prov in gazetteers.words('caprovinces.txt')] +
+                          [country for country in gazetteers.words('countries.txt')] + 
+                          [country for country in gazetteers.words('isocountries.txt')] + 
+                          [city for city in gazetteers.words('uscities.txt')] + 
+                          [abbrev for abbrev in gazetteers.words('usstateabbrev.txt')] + 
+                          [state for state in gazetteers.words('usstates.txt')])
     
     def get_word_counts(self):
         with open(self.corpus) as corpus:
@@ -51,13 +62,25 @@ class FeatureExtractor(object):
                     and not next.isupper() else None)
         else:
             return None
+
+    def name(self, word, prev=None, next=None):
+        return "NAME" if word in self.names else None
+
+    def place(self, word, prev=None, next=None):
+        return "PLACE" if word in self.places else None
+
+    def POSTag(self, line):
+        #TODO: get Stanford POS Tagger working here
     
     def features(self, word, prev, next):
         """Get the list of feature strings for `word`"""
         feat_fns = [self.init_caps,
                     self.all_caps,
                     self.next_init_caps,
-                    self.next_all_caps]
+                    self.next_all_caps,
+                    self.name,
+                    self.place,
+                    ]
         feat_strings = [feat_fn(word, prev, next) for feat_fn in feat_fns]
         return [feat for feat in feat_strings if feat is not None]
 
@@ -66,7 +89,7 @@ class FeatureExtractor(object):
             # reads the whole corpus into memory unfortunately
             lines = [line.strip() for line in corpus.readlines()
                      if line.strip() is not '']
-            print lines
+            #print lines
             for i, line in enumerate(lines):
                 word, label = line.split()
                 if i-1 >= 0:
